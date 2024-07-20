945573c7ac4d2c43134f31b239f81115a583f5af
Kays <106269338+Kays8@users.noreply.github.com>
2024-07-20 19:44:43 +0700

    Update index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = 5000; // ตั้งค่าพอร์ตเป็น 5000
 
 app.get('/', (req,res)=>{
     res.send('<h1>Hello Restaurant API<h1/>');
